1c9921f5928467041f0fd53706e1bb84571edd4f
WilsonC67 <wilson.chen@quinnipiac.edu>
2025-02-19 19:36:51 -0500

    Added a query for either a dog or cat, then printed lines corresponding to the user's selection.

---
+++ AnimalPicker.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AnimalPicker {
+	public static void main(String[] args) {
+		
+		String animal;
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("Hello! Would you like to see a dog or cat?");
+		
+		// possibly can implement switch statement here
+		animal = userInput.nextLine().toUpperCase();
+		
+		
+		if (animal.equals("CAT")) {
+			System.out.println("Cat ASCII art here:");
+		} else if (animal.equals("DOG")) {
+			System.out.println("Dog ASCII art here:");
+		}
+		
+		
+		
+	}
+}
